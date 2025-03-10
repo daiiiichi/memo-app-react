820283b214fc0d12a184cc1a0a7af9df4c793962
daichitoyohara <daiiiichit@gmail.com>
2025-03-10 22:14:34 +0900

    メモの一覧表示の実装

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MemoList } from "./components/memoList.tsx";
 import { MemoEdit } from "./components/memoEdit.tsx";
 
 function App() {
+  const [memos, setMemos] = useState([]);
   const [selectedMemo, setSelectedMemo] = useState(null);
+
+  useEffect(() => {
+    const memoList = JSON.parse(localStorage.getItem("memos")) || [];
+    setMemos(memoList);
+  }, []);
 
   return (
     <div className="App">
       <h2>{selectedMemo ? "編集" : "一覧"}</h2>
       <main>
         <div className="container">
-          <MemoList props={{ selectedMemo, setSelectedMemo }} />
+          <MemoList props={{ memos, selectedMemo, setSelectedMemo }} />
         </div>
         <div className="container">
           <MemoEdit />
--- src/components/memoList.tsx
+++ src/components/memoList.tsx
@@ -5,6 +5,15 @@ export const MemoList = ({ props }) => {
   return (
     <div>
       <strong className="">memolist</strong>
+      {props.memos.map((memo) => (
+        <p
+          key={memo.id}
+          className={styles.newMemo}
+          onClick={() => props.setSelectedMemo(memo)}
+        >
+          {memo.text}
+        </p>
+      ))}
       <p
         className={styles.newMemo}
         onClick={() => props.setSelectedMemo({ id: 0, text: "" })}
